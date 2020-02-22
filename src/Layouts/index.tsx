8b32b8b2989666c2ec256eaa2ad3bd0caaae81e1
Daniil Maslov <s0xzwasd@protonmail.com>
2020-02-22 18:34:32 +0300

    feat: add basic components markup

---
+++ src/Layouts/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Input from '../components/Input';
+
+const Layout: React.SFC = () => {
+  return (
+    <div>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </div>
+  );
+};
+
+export default Layout;
