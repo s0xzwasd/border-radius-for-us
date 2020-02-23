07224447a0e7d09a298dcc9cc05d16c8df88faae
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-02-23 22:36:59 +0300

    feat: add new dumb components (#17)

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -1,16 +1,3 @@
-import React from 'react';
+import Main from './Main';
 
-import Input from '../components/Input';
-
-const Layout: React.SFC = () => {
-  return (
-    <div>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </div>
-  );
-};
-
-export default Layout;
+export default Main;
