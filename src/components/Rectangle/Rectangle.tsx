23885f3ab5d1eef65f777a776574e24062e00dfd
Daniil Maslov <29286703+s0xzwasd@users.noreply.github.com>
2020-02-23 19:22:21 +0300

    [B-4] Add basic components markup (#8)
    * feat: add basic components markup
    
    * fix: delete css import
    
    * fix: add install on Travis CI
    
    * fix: delete test script

---
+++ src/components/Rectangle/Rectangle.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Rectangle.module.css';
+
+const Input: React.SFC = () => {
+  return <input className={styles.input} />;
+};
+
+export default Input;
---
+++ src/components/Rectangle/index.tsx
@@ -0,0 +1,3 @@
+import Input from './Rectangle';
+
+export default Input;
