74ee3b90ea2d6b270b1cadc3c0daca0be69490c1
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-01 16:32:48 +0300

    feat: add clipboard implementation

--- src/components/Clipboard/Clipboard.tsx
+++ src/components/Clipboard/Clipboard.tsx
@@ -13,24 +13,26 @@ class Clipboard extends React.PureComponent<Props> {
     super(props);
   }
 
-  textArea?: Record<string, any> | null | undefined;
+  textArea: any;
 
   copyToClipboard = (e: any | null | undefined) => {
-    // @ts-ignore
     this.textArea.select();
     document.execCommand('copy');
     // This is just personal preference.
     // I prefer to not show the the whole text area selected.
-    e.target.focus();
-    this.setState({ copySuccess: 'Copied!' });
+    // e.target.focus();
+    // this.setState({ copySuccess: 'Copied!' });
   };
 
   render() {
     const { currentValue } = this.props;
     return (
       <div>
-        <input ref={textarea => (this.textArea = textarea)} type="text" value="some-value" />
-        <section className={styles.clipboard}>border-radius: {currentValue}px;</section>
+        <input
+          ref={textarea => (this.textArea = textarea)}
+          className={styles.clipboard}
+          value={`border-radius: ${currentValue}px;`}
+        ></input>
         <button type="button" className={styles.button} onClick={this.copyToClipboard}>
           Copy to clipboard
         </button>
